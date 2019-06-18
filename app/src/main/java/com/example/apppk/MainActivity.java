74054c8d673350453e0ec20547cab9e48b3ccc50
PyaePhyoKyaw <pyaephyokyaw.22121996@gmail.com>
2019-06-18 15:49:36 +0630

    Init

--- app/src/main/java/com/example/apppk/MainActivity.java
+++ app/src/main/java/com/example/apppk/MainActivity.java
@@ -3,14 +3,11 @@ package com.example.apppk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.d("General", "onCreate: Success");
     }
 }
